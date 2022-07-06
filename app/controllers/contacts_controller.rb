bd8c99ac842e7d2fdd4107618fad54159076c98e
fqqk <fqqk.work68@gmail.com>
2022-07-06 14:55:41 +0900

    お問い合わせ用コントローラー作成

---
+++ app/controllers/contacts_controller.rb
@@ -0,0 +1,13 @@
+class ContactsController < ApplicationController
+  def new
+  end
+
+  def confirm
+  end
+
+  def back
+  end
+
+  def create
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  get 'contacts/new'
+  get 'contacts/confirm'
+  get 'contacts/back'
+  get 'contacts/create'
   resources :blogs
 end
