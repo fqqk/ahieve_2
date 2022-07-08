edc340269ab1481cc92e5757e4861ab74374c73d
fqqk <fqqk.work68@gmail.com>
2022-07-08 15:33:52 +0900

    ストロングパラメータ実装

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,13 +1,24 @@
 class ContactsController < ApplicationController
   def new
+    @contact = Contact.new
   end
 
   def confirm
+    @contact = Contact.new(contact_params)
+    if @contact.invalid?
+      render new
+    end
   end
 
   def back
   end
 
   def create
+  end
+
+  private
+  
+  def contact_params
+    params.require(:contact).permit(:name,:email,:category,:content)
   end
 end
