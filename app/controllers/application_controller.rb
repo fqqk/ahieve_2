4fa50d012fd6b10ec4a2aa85db5e699c01d3e138
fqqk <fqqk.work68@gmail.com>
2022-07-15 10:12:13 +0900

    追加のストロングパラメータ

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,8 +4,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name]) # 新規登録時(sign_up時)にnameというキーのパラメーターを追加で許可する
-    devise_parameter_sanitizer.permit(:account_update, keys: [:name]) #  更新時(update時)にnameというキーのパラメーターを追加で許可する
-    devise_parameter_sanitizer.permit(:account_update, keys: [:icon]) #  更新時(update時)にiconというキーのパラメーターを追加で許可する
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name]) # 新規登録時にnameというキーのパラメーターを追加で許可する
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name]) #  更新時にnameというキーのパラメーターを追加で許可する
   end
 end
