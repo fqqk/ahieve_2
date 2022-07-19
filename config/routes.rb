8937cc768c14771f78efff863ed3c2c1bee6036a
fqqk <fqqk.work68@gmail.com>
2022-07-19 14:50:24 +0900

    トップページ追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
-  root to: "index"
+  root to: 'homes#index'
   get 'users/index'
   get 'users/show'
-  root to: 'blogs#index'
   devise_for :users
   post 'contacts/confirm', to: 'contacts#confirm', as: 'confirm'
   post 'contacts/back', to: 'contacts#back', as: 'back'
