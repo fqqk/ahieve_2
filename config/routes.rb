955807662a4ba1fb903a9e9cb616dcb7cdf9ace7
fqqk <fqqk.work68@gmail.com>
2022-07-22 14:47:07 +0900

    インデント整えた

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,7 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
+  root 'blogs#index'
   get 'users/index'
   get 'users/show'
-=======
-  root 'blogs#index'
->>>>>>> login_system
   devise_for :users
   post 'contacts/confirm', to: 'contacts#confirm', as: 'confirm'
   post 'contacts/back', to: 'contacts#back', as: 'back'
