d8cb3bbde9e4289d747af74e93d84b6f88fa1a72
fqqk <fqqk.work68@gmail.com>
2022-07-26 17:27:30 +0900

    commentモデル作成とuserモデルに1:多commentの関係を紐付け

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_many :blogs, dependent: :destroy
+  has_many :comments, dependent: :destroy
   paginates_per 2
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :trackable, :confirmable
