1a79399c9b07b25da046ccb654a105222cc13b0f
fqqk <fqqk.work68@gmail.com>
2022-07-26 10:20:52 +0900

    deviceのregistration_pathがsession_pathになっていたので変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,4 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :trackable, :confirmable
   has_one_attached :icon
-  validates :name, presence: true
 end
