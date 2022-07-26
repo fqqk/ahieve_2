eac4c076273ea0c171f19447081f2fd056fd444c
fqqk <fqqk.work68@gmail.com>
2022-07-26 17:11:13 +0900

    user一覧のページネーションの実装(includesメソッドどこで使う？)

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,13 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_many :blogs, dependent: :destroy
+  paginates_per 2
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :trackable, :confirmable
   has_one_attached :icon
+
+  scope :index_all, -> {
+    select(:id, :name, :email)
+    .order(id: :asc)
+  }
 end
