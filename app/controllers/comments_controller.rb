67dc0760b54c957a998d4f3b64cdf0a1cd7f6e0d
fqqk <fqqk.work68@gmail.com>
2022-07-27 14:20:58 +0900

    ブログ投稿機能、削除機能実装のリファクタ

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,33 +1,46 @@
 class CommentsController < ApplicationController
-  before_action :set_comment, only: %i[destroy]
+  before_action :set_comment,:set_blog, only: %i[ destroy ]
 
   def create
-    blog = Blog.find(params[:blog_id])
+    #リファクタ中
+    # @blog = Blog.find(params[:blog_id])
+
     comment = current_user.comments.new(comment_params)
-    comment.blog_id = blog.id
-    comment.save
-    respond_to do |format|
-      format.html { redirect_to blog_path(blog), notice: 'Comment was successfully created.' }
-      format.json { head :no_content }
+    # comment.blog_id = @blog.id
+    if comment.save
+      respond_to do |format|
+        format.html { redirect_back fallback_location: root_path, notice: 'Comment was successfully created.' }
+        format.json { head :no_content }
+      end
+    else
+      redirect_to root_path
     end
   end
 
   def destroy
-    @comment.destroy
-    # Comment.find_by(id: params[:id], blog_id: params[:blog_id]).destroy
-    respond_to do |format|
-      format.html { redirect_to blog_path(params[:blog_id]), notice: 'Comment was successfully destroyed.' }
-      format.json { head :no_content }
+    if @comment.destroy
+      respond_to do |format|
+
+        format.html { redirect_to blog_path(@blog), notice: 'Comment was successfully destroyed.' }
+        format.json { head :no_content }
+      end
+    else
+      redirect_to root_path
     end
   end
 
   private
+
+  def set_blog
+    @blog = Blog.find(params[:blog_id])
+  end
 
   def set_comment
     @comment = Comment.find_by(id:params[:id], blog_id: params[:blog_id])
   end
 
   def comment_params
-    params.require(:comment).permit(:comment)
+    params.require(:comment).permit(:comment, :blog_id)
   end
+
 end
