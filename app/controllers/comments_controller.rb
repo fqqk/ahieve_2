4b4027b211dac94f2242bb922a5da3949dd2526b
fqqk <fqqk.work68@gmail.com>
2022-07-27 10:21:13 +0900

    before_actionを定義してコード分割

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,6 @@
 class CommentsController < ApplicationController
+  before_action :set_comment, only: %i[destroy]
+
   def create
     blog = Blog.find(params[:blog_id])
     comment = current_user.comments.new(comment_params)
@@ -11,7 +13,8 @@ class CommentsController < ApplicationController
   end
 
   def destroy
-    Comment.find_by(id: params[:id], blog_id: params[:blog_id]).destroy
+    @comment.destroy
+    # Comment.find_by(id: params[:id], blog_id: params[:blog_id]).destroy
     respond_to do |format|
       format.html { redirect_to blog_path(params[:blog_id]), notice: 'Comment was successfully destroyed.' }
       format.json { head :no_content }
@@ -19,6 +22,10 @@ class CommentsController < ApplicationController
   end
 
   private
+
+  def set_comment
+    @comment = Comment.find_by(id:params[:id], blog_id: params[:blog_id])
+  end
 
   def comment_params
     params.require(:comment).permit(:comment)
