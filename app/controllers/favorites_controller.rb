3563d0fef66d65944b02b849c5419ffd3318bcf8
fqqk <fqqk.work68@gmail.com>
2022-08-03 12:43:07 +0900

    ブログお気に入り一覧の実装

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,6 +1,10 @@
 class FavoritesController < ApplicationController
   before_action :authenticate_user!
 
+  def index
+    @favorites = current_user.favorite_blogs
+  end
+
   def create
     favorite = current_user.favorites.create(blog_id: params[:blog_id])
     redirect_to blogs_url, notice: "#{favorite.blog.user.name}さんのブログをお気に入り登録しました"
--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,6 @@ Rails.application.routes.draw do
   resources :blogs do
     resources :comments, only:[:create, :destroy, :update, :edit]
   end
-  resources :favorites, only: [:create, :destroy]
+  resources :favorites, only: [:create, :destroy, :index]
   mount LetterOpenerWeb::Engine, at: '/letter_opener' if Rails.env.development?
 end
